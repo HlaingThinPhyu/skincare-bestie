368144af7929bd38e025b22706e3be3dc3907a81
Thinn <65583879+HlaingThinPhyu@users.noreply.github.com>
2026-07-26 09:30:05 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} ${notoMyanmar.variable} h-full antialiased`}
     >
       <head>
-        <!-- Privacy-friendly analytics by Plausible -->
+        
         <script async src="https://plausible.io/js/pa-joFXTgVXaa1z8tGLLQDbN.js"></script>
         <script>
           window.plausible=window.plausible||function(){(plausible.q=plausible.q||[]).push(arguments)},plausible.init=plausible.init||function(i){plausible.o=i||{}};
